861a08cf05e0e684052ae4b1e1dfcc5008990516
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-07-06 23:49:08 -0400

    Added access controls.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(pairsOfCards: numberOfPairsOfCards)
+    private lazy var game = Concentration(pairsOfCards: numberOfPairsOfCards)
     
     var numberOfPairsOfCards: Int {
         return (cardButtons.count + 1) / 2
     }
     
-    var flipCount: Int = 0 {
+    private(set) var flipCount: Int = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
 
     
-    @IBOutlet weak var flipCountLabel: UILabel!
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         
         if let cardNumber = cardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -44,10 +44,10 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["👻", "🎃", "🍏", "🍆","🥝","🎾","🏀","🏉","🌽", "🍜", "🦈", "🐇", "🍀", "🌏", "🚜", "🏔", "🛰", "⌚️", "⏱", "🏝", "🦊",]
-    var emoji = [Int: String]()
+    private var emojiChoices = ["👻", "🎃", "🍏", "🍆","🥝","🎾","🏀","🏉","🌽", "🍜", "🦈", "🐇", "🍀", "🌏", "🚜", "🏔", "🛰", "⌚️", "⏱", "🏝", "🦊",]
+    private var emoji = [Int: String]()
     
-    func emoji(for card: Card) -> String {
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
