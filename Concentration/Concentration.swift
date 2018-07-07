44578cddc8a2e02abc329d717ca6102199c17fdf
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-07-06 20:18:48 -0400

    Added computed property version of indexOfOneAndOnlyFaceUpCard. [This is where the actual changes begin. The first 3 commits were a mistake.]

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -34,16 +34,10 @@ class Concentration
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
                 //Either no cards, or two cards face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
-            
         }
         
 //        if cards[index].isFaceUp {
